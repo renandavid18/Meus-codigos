e83ec7b5b38057d684515698e0558eab49398568
renandavid18 <renan2310.david@gmail.com>
2026-03-31 21:33:18 +0000

    31 de mar. de 2026, 18:33

--- Calculadora_V2.c
+++ Calculadora_V2.c
@@ -8,6 +8,15 @@ int main(void){
     printf("Bem vindo à Calculadora V.2 \n");
     sytem(pause);
     
-    printf("=========================== MENU =========================== \n")
-    prinf("Selecione Qual calculo Deseja!")
+    printf("=========================== MENU =========================== \n");
+    prinf("Qual cálculo Deseja ultilizar?");
+    printf("(1) Soma");
+    printf("(2) Subtração");
+    printf("(3) Multiplicação");
+    printf("(4) Divisão");
+    printf("(5) Radiciação");
+    printf("(6) Exponenciação");
+    printf("(7) S");
+    printf("(8) Soma");
+
 }
