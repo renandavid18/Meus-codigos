334c46da6032636523c0ae5346213c7f4031023f
renandavid18 <renan2310.david@gmail.com>
2026-03-31 21:30:21 +0000

    31 de mar. de 2026, 18:30

---
+++ Calculadora_V2.c
@@ -0,0 +1,13 @@
+#include<stdlib.h>
+#include<locale.h>
+#include<stdio.h>
+
+int main(void){
+    setlocale(LC_CTYPE, "Portuguese");
+    
+    printf("Bem vindo à Calculadora V.2 \n");
+    sytem(pause);
+    
+    printf("=========================== MENU =========================== \n")
+    prinf("Selecione Qual calculo Deseja!")
+}
