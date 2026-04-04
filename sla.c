1765841c79efb3c7cb2d144e476e4b991a093535
renandavid18 <renan2310.david@gmail.com>
2026-04-04 23:37:57 +0000

    4 de abr. de 2026, 20:37

--- sla.c
+++ sla.c
@@ -20,6 +20,7 @@
             switch(op) {
                 
                 case 0:
+                    
                     printf("Complete com o valor da tabuada que você deseja \n");
                     printf("Tabuada de 1 á ");
                     scanf("%d", &v);
@@ -28,18 +29,60 @@
                     
                     printf("A tabuada de %d é", vlt);
                     for(j=0; j<=v; j++){
-                        k = j * v;
-                        printf("%d X %d = %d \n", v, j, k);
+                        k = j * vlt;
+                        printf("%d X %d = %d \n", vlt1, j, k);
                     }
+                    
                     system("pause");
-                    printf("Deseja continuar? \n");
-
-
-
-
+                    
+                    while(d = 1){
+                        printf("Deseja continuar? \n");
+                        printf("(1) Sim | (0) Não \n");
+                        scanf("%d", &c);
+                        if(c = 0){
+                            i = 1;
+                            d = 1;
+                        }
+                        if(c = 1){
+                            i = 0;
+                            d = 1;
+                        }else{
+                            d = 0;
+                        }
+                    } 
                 break;
 
                 case 1:
+
+                    printf("Complete com o valor da tabuada que você deseja \n");
+                    printf("Tabuada de 1 á ");
+                    scanf("%d", &v);
+                    printf("Me informa o o valor que vocÊ deseja ver a tabuada: \n");
+                    scanf("%f", &vlt1);
+                    
+                    printf("A tabuada de %f é", vlt1);
+                    for(j=0; j<=v; j++){
+                        k = j * vlt1;
+                        printf("%d X %d = %d \n", vlt1, j, k);
+                    }
+                    
+                    system("pause");
+                    
+                    while(d = 1){
+                        printf("Deseja continuar? \n");
+                        printf("(1) Sim | (0) Não \n");
+                        scanf("%d", &c);
+                        if(c = 0){
+                            i = 1;
+                            d = 1;
+                        }
+                        if(c = 1){
+                            i = 0;
+                            d = 1;
+                        }else{
+                            d = 0;
+                        }
+                    } 
 
             }
         }   
