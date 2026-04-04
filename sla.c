5d94ed54b2269f8b6e63e572ba1f6f39cbaada37
renandavid18 <renan2310.david@gmail.com>
2026-04-04 22:20:57 +0000

    4 de abr. de 2026, 19:20

--- sla.c
+++ sla.c
@@ -4,8 +4,25 @@
     
     int main() {
         
-        setlocale(LC_CTYPE, "Portuguese");
+        int op, vlt, n;  // vlt = valor da tabuada
+        double vlt2, n;
 
-        
+        setlocale(LC_CTYPE, "Portuguese");
+        printf("Aqui Iremos mostrar a tabuada de um número qualquer! \n");
+
+        printf("Para começar você deseja fazer tabuada de um numero com virgula?")
+        printf("(1) Sim | (0) Não (escolher por anos) \n"); 
+        scanf("%d", op);
+
+        while (i != 1){  
+             
+            switch {
+
+            }
+            printf("Digite o valor da tabuada que você deseja");
+            printf("Se digitar 30 vai ter uma tabuada de 1 á 30 se 2 vai ter de 1 á 2.\n");
+            scanf("vlt");
+        }
+
         
     }
