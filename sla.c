7013ecf09a81c045f0a9d7f4393c87b3b648ef6a
renandavid18 <renan2310.david@gmail.com>
2026-04-04 23:04:55 +0000

    4 de abr. de 2026, 20:04

--- sla.c
+++ sla.c
@@ -19,14 +19,15 @@
 
             switch (op) {
                 
-                case 1:
+                case 0:
                     printf("Digite o valor da tabuada que você deseja");
                     printf("Se digitar 30 vai ter uma tabuada de 1 á 30 se 2 vai ter de 1 á 2.\n");
                     scanf("vlt");
 
+                break;
+
+                case 1:
+
             }
-
-        }
-
-        
+        }   
     }
