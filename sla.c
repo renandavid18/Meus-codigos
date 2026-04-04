a450d30cacb3a5ae99c5692d39fd1d455912e5e2
renandavid18 <renan2310.david@gmail.com>
2026-04-04 23:12:44 +0000

    4 de abr. de 2026, 20:12

--- sla.c
+++ sla.c
@@ -5,25 +5,25 @@
     int main() {
         
         int op, i, vlt, n;  // vlt = valor da tabuada
-        double vlt2, n;
+        
 
         setlocale(LC_CTYPE, "Portuguese");
-        i = 1
+        i = 0;
 
-        while (i != 1){  
+        while(i != 1) {  
             
             printf("Aqui Iremos mostrar a tabuada de um número qualquer! \n");
-            printf("Para começar você deseja fazer tabuada de um numero com virgula?")
+            printf("Para começar você deseja fazer tabuada de um numero com virgula?");
             printf("(1) Sim | (0) Não (Numero Inteiro) \n"); 
             scanf("%d", op);
 
-            switch (op) {
+            switch(op) {
                 
                 case 0:
-                    printf("Digite o valor da tabuada que você deseja");
-                    printf("Se digitar 30 vai ter uma tabuada de 1 á 30 se 2 vai ter de 1 á 2.\n");
+                    printf("Complete com o valor da tabuada que você deseja");
+                    printf("Tabuada de 1 á ");
                     scanf("%d", vlt);
-                    
+
 
                 break;
 
