ea8f2887b26d19a82f1af075d3391159dfc46e69
renandavid18 <renan2310.david@gmail.com>
2026-04-05 21:38:59 +0000

    5 de abr. de 2026, 18:38

---
+++ Teste break.c
@@ -0,0 +1,28 @@
+#include<stdlib.h>
+#include<locale.h>
+#include<stdio.h>
+
+int main(void){
+    
+    int i, j, k;
+    printf("A soma de 50 valores é: \n");
+    k = 0;
+
+    for(i=1;i<=100;i++){
+        
+        j = i + k;
+       printf("%d", j);
+        
+        k = j;
+
+        if(i == 51){
+
+            break;
+
+        }
+        
+        printf("Aqui estão os resultados!");
+        system("pause");
+
+    }
+}
