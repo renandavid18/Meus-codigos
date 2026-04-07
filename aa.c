a166bc48be6d72388395b88dfe5b71ba33e88fa7
renandavid18 <renan2310.david@gmail.com>
2026-04-07 23:11:34 +0000

    7 de abr. de 2026, 20:11

--- aa.c
+++ aa.c
@@ -690,7 +690,7 @@
                          
                          system("cls"); 
                          printf("  ===================================================  \n");
-                         printf("                CAIXA ECONÔMICA FEDERAL                \n");
+                         printf("                CAIXA ECONOMICA FEDERAL                \n");
                          printf("  ===================================================  \n");
                          
                          for(q = 0; q <= 100; q++) {
@@ -862,9 +862,9 @@
                          
                          system("cls"); 
                          printf("  ===================================================  \n");
-                         printf("                  BANCO DO BRASIL                      \n");
+                         printf("                   BRADESCO S.A.                       \n");
                          printf("  ===================================================  \n");
-                         
+                                             
                          for(q = 0; q <= 100; q++) {
                               printf("\r[+] Sincronizando dados bancários: %d%%", q);
                               fflush(stdout); 
