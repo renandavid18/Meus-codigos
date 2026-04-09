c3a456687cdebe2545f1210ec137f06e9fb18b42
renandavid18 <renan2310.david@gmail.com>
2026-04-09 21:11:48 +0000

    9 de abr. de 2026, 18:11

--- Tabela_ecola.c
+++ Tabela_ecola.c
@@ -7,7 +7,7 @@
         setlocale(LC_CTYPE, "Portuguese");
         
         char alunos[2][60]={};
-        double nota[2]={};
+        double nota1[2]={}, nota2[2]={}, nota3[2]={}, nota4[2]={};
         int i, j;
         
         printf("Criaremos uma tabela de alunos com suas notas: \n");
@@ -18,18 +18,28 @@
             printf("Digite o nome do aluno nº: %d\n", i+1);
             fgets(alunos[i], 40, stdin);
 
-            printf("Agora me informa a nota no %dº bimestre do aluno nº: %d\n", i+1, i+1);
+            printf("Agora me informa a nota no 1º bimestre do aluno nº: %d\n", i+1);
+            scanf("%lf", nota);
+
+            printf("Agora me informa a nota no 2º bimestre do aluno nº: %d\n", i+1);
+            scanf("%lf", nota);
+
+            printf("Agora me informa a nota no 3º bimestre do aluno nº: %d\n", i+1);
+            scanf("%lf", nota);
+
+            printf("Agora me informa a nota no 4º bimestre do aluno nº: %d\n", i+1);
             scanf("%lf", nota);
 
             fflush(stdin); //limpa qualquer ENTER que houver
 
         }
 
-        printf("| Nome dos Alunos                                              | 1º bimestre | 2º bimestre | Média bimestral | 3º bimestre | 4º bimestre | Média final |\n");
+        printf("| Nome dos Alunos                                              | 1º bimestre | 2º bimestre | Média Parcial | 3º bimestre | 4º bimestre | Média final |\n");
         
         for(j=0;j<=19;j++){
 
-            printf("| %60s | %11.2lf")
+            mp = nota1[j] + nota2[]
+            printf("| %60s | %9.2lf | %9.2lf | %11.2lf | %9.2lf | %9.2lf | %9.2lf | " alunos[j], nota1[j], nota2[j], m, nota3[j], nota4[j], mf);
             
 
         }
