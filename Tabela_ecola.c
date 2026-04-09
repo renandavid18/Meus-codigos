8f5e34e09d9b2d59a3aee8352954df3e8b445ef6
renandavid18 <renan2310.david@gmail.com>
2026-04-09 20:56:41 +0000

    9 de abr. de 2026, 17:56

---
+++ Tabela_ecola.c
@@ -0,0 +1,34 @@
+ #include <stdio.h>
+    #include <stdlib.h>
+    #include <locale.h>
+
+    int main(void){
+
+        setlocale(LC_CTYPE, "Portuguese");
+        
+        char alunos[2][60]={};
+        double nota[2]={};
+        int i, j;
+        
+        printf("Criaremos uma tabela de alunos com suas notas: \n");
+        printf("Limite 5 alunos\n");
+        
+        for(i=0;i<=1;i++){
+            
+            printf("Digite o nome do aluno nº: %d\n", i+1);
+            fgets(alunos[i], 40, stdin);
+            fflush(stdin);
+
+            printf("Agora me informa a nota no 1º bimestre do aluno nº: %d\n", i+1);
+            scanf("%lf", nota);
+
+        }
+
+         printf("Nome dos Alunos                                             | 1º bimestre | 2º bimestre | Média bimestral | 3º bimestre | 4º bimestre | Média final |\n");
+        for(j=0;j<=19;j++){
+
+           
+        }
+
+        system("pause");
+    }
